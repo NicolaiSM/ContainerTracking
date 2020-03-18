66d35badd73eefcd339e4886e189170a6669a842
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-03-18 16:59:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- Hallo world/src/githuberlort.java
+++ Hallo world/src/githuberlort.java
@@ -1,6 +1,7 @@
 
-public class githuberlort {
+public class doinYourMom {
+	String a = "a";
 	public static void main(String[] args) {
-		System.out.println("fish dicks 2");
+		System.out.println(a);
 	}
 }
