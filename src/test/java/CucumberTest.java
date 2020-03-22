2b4f682fc7e5cb14353293d66b288890c8b5fb2a
William Bendix Pedersen <william.bendix@live.dk>
2020-03-22 13:42:37 +0100

    Cucumber opdateringer (POM fil edited)

---
+++ src/test/java/CucumberTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.runner.RunWith;
+import io.cucumber.CucumberOptions;
+import io.cucumber.junit.cu
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/main/resources/")
+
+public class CucumberTest {
+
+}
