6f689332113783e2f38f572b2b5b871edb370de8
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-03-22 15:44:02 +0100

    h

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -1,8 +1,8 @@
 
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
-import io.cucumber.java.en.Then;
-import static org.junit.Assert.*;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
