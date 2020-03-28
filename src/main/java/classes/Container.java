7bf2fe8f3a1f79ab41f57b0e74884d85ce5ad8df
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-03-28 09:09:19 +0100

    creation of Client, Container, ID and Journey Class

---
+++ src/main/java/classes/Container.java
@@ -0,0 +1,30 @@
+package classes;
+
+
+
+class Container {
+	
+	// Should port and destination be in journey instead
+	
+	private String PortOfOrigin;
+	private String Destination;
+	private String Content;
+	private Client user;
+	private Journey journey;
+	private ID id;
+	
+	
+	public Container(String portOfOrigin, ID id) {
+		PortOfOrigin = portOfOrigin;
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
